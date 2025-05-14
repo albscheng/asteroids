251f2c060ffe8ecb77f0fec7f2d778a1516afe51
albscheng <albscheng@gmail.com>
2025-05-13 21:34:03 -0700

    mvp

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
+import sys
 import pygame
 
 def main():
@@ -9,6 +14,27 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # init clock
+    c = pygame.time.Clock()
+    dt = 0
+
+    # Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
+    # spawn player
+    p = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # spawn asteroid field
+    af = AsteroidField()
+
     # Game loop
     while True:
         for event in pygame.event.get():
@@ -17,8 +43,28 @@ def main():
 
         # Fill the screen black
         screen.fill((0, 0, 0))
+
+        # update updatables group
+        updatable.update(dt)
+
+        # check for collisions
+        for a in asteroids:
+            if p.collides_with(a):
+                sys.exit("Game over!")
+
+        for a in asteroids:
+            for bullet in shots:
+                if bullet.collides_with(a):
+                    a.split()
+                    bullet.kill()
+
+        # draw drawables group
+        for d in drawable:
+            d.draw(screen)
         # refresh the screen
         pygame.display.flip()
+
+        dt = (c.tick(60)/1000)
 
 
 if __name__ == "__main__":
